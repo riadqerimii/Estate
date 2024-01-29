220494772e7395749f663c572f1be1353ffcd51a
riadqerimii <123258318+riadqerimii@users.noreply.github.com>
2024-01-29 17:43:24 +0100

    create a middleware and handle errors

--- api/index.js
+++ api/index.js
@@ -23,6 +23,16 @@ app.listen(3000, () => {
 });
 
 // path
-
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// handling error and path
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
