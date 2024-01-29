442b441c837578b2e9734d3e65a67e928dbb2357
riadqerimii <123258318+riadqerimii@users.noreply.github.com>
2024-01-29 16:46:13 +0100

    create sign up route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import userRouter from "./routes/user.route.js";
-
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -16,11 +16,13 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3030!");
+  console.log("Server is running on port 3000");
 });
 
 // path
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
