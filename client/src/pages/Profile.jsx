83e1c571bc47d82e80fed649fca1336b3426fe84
riadqerimii <123258318+riadqerimii@users.noreply.github.com>
2024-01-31 02:28:06 +0100

    complete user listing delete

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -141,10 +141,29 @@ function Profile() {
       setShowListingsError(true);
     }
   };
+  // delete listint
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prevState) =>
+        prevState.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="max-w-6xl mx-auto flex flex-col lg:flex-row gap-10 mt-10 p-3">
-      <div className="w-full ">
+      <div className="w-1/2 ">
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
             onChange={(e) => setFile(e.target.files[0])}
@@ -235,40 +254,45 @@ function Profile() {
           <p>{showListingsError ? "Error showing listings" : ""}</p>
         </div>
       </div>
-      <div className="w-full">
-        {userListings &&
-          userListings.length > 0 &&
-          userListings.map((listing) => (
-            <Link
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col w-1/2 gap-4">
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
               key={listing._id}
-              to={`/listing/${listing._id}`}
-              className="flex flex-col mt-3"
+              className="border rounded-lg p-3 flex w-full justify-between items-center gap-4"
             >
-              <h1 className="text-zinc-800 font-semibold text-3xl my-5 self-center">
-                Your Listings
-              </h1>
-              <div
-                className="flex justify-between items-center shadow-lg
-              hover:bg-red-100 
-              border
-            "
-              >
+              <Link to={`/listing/${listing._id}`}>
                 <img
                   src={listing.imageUrls[0]}
-                  alt="images  of the listing"
-                  className="h-24 w-24 object-contain"
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
                 />
-                <p className="font-medium text-slate-800 truncate">
-                  {listing.name}
-                </p>
-                <div className="flex flex-col gap-2">
-                  <button className="bg-green-700 text-white p-2">Edit</button>
-                  <button className="bg-red-700 text-white p-2">Delete</button>
-                </div>
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold  hover:underline truncate flex-1"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className="flex flex-col item-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className="text-green-700 uppercase">Edit</button>
+                </Link>
               </div>
-            </Link>
+            </div>
           ))}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
