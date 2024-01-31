e7b2c93ce2bc2a6b892d012088cfa98675626be6
riadqerimii <123258318+riadqerimii@users.noreply.github.com>
2024-01-31 16:50:20 +0100

    update header search

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 function Header() {
+  // States
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-slate-300 p-2">
       <nav className="flex justify-between max-w-7xl mx-auto">
@@ -13,13 +34,20 @@ function Header() {
             <span className="text-slate-400">ESTATE</span>
           </h1>
         </Link>
-        <form className=" bg-slate-100 p-2 rounded-md flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className=" bg-slate-100 p-2 rounded-md flex items-center"
+        >
           <input
             className="bg-transparent focus:outline-none w-24 sm:w-64"
             type="text"
             placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <div className="flex gap-3">
           <Link
